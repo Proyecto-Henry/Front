0bfbbb5e24cc723692bea8e95633c5c5793a69b8
Tomas <tomiiulman@gmail.com>
2025-04-25 18:48:00 -0300

    Estructura de página de bienvenida

--- pf-henry/src/app/page.tsx
+++ pf-henry/src/app/page.tsx
@@ -1,9 +1,70 @@
-// import Image from "next/image";
+import Image from 'next/image';
+import { Plus, Building, Boxes, LineChart } from 'lucide-react';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Home
-    </div>
+    <main>
+      <section>
+
+        <div>
+          <div>
+            <h1>
+              BIENVENIDO A <span>SafeStock</span>
+            </h1>
+            <p>
+              Donde puedes gestionar tus sucursales y stock de manera rápida, eficiente y cómoda
+            </p>
+          </div>
+
+          <div>
+            <Image 
+              src="/homeImg.png"
+              alt="Ilustración gestión de stock"
+              width={600}
+              height={600}
+            />
+          </div>
+        </div>
+
+        <div>
+          <h2>¿Qué puedes hacer?</h2>
+
+          <div>
+            <div>
+              <Plus size={32} />
+              <p>
+                AGREGAR SUCURSALES
+              </p>
+            </div>
+            <div>
+              <Building size={32} />
+              <p>
+                ADMINISTRAR SUCURSALES
+              </p>
+            </div>
+            <div>
+              <Boxes size={32} />
+              <p>
+                GESTIONAR STOCK
+              </p>
+            </div>
+            <div>
+              <LineChart size={32} />
+              <p>
+                CONTROLAR VENTAS
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <p>¿No tienes cuenta?</p>
+          <a href="#">
+            Crea una gratis o elige el plan premium
+          </a>
+        </div>
+
+      </section>
+    </main>
   );
 }
