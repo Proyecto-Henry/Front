86f9cdda63862baeb4f5b1535cc435f6679f63fb
Elias Arrieta Hernandez <elias@MacBook-Air.local>
2025-05-14 00:33:42 -0500

    Arreglos 2.1

--- pf-henry/src/app/sucursal/[id]/page.tsx
+++ pf-henry/src/app/sucursal/[id]/page.tsx
@@ -1,15 +1,19 @@
-import { StoreService } from '@/services/storeService';
-import Sucursal from '@/components/Sucursal';
-import { notFound } from 'next/navigation';
-import { IPageProps } from '@/interfaces/interfaces';
+import { StoreService } from "@/services/storeService";
+import Sucursal from "@/components/Sucursal";
+import { notFound } from "next/navigation";
+// import { IPageProps } from '@/interfaces/interfaces';
+interface PageProps {
+  params: {
+    id: string;
+  };
+}
 
-
-export default async function Page({ params: { id } }: IPageProps) {
+export default async function Page({ params: { id } }: PageProps) {
   try {
     const productos = await StoreService.getProducts(id);
-    return <Sucursal productos={productos} store_id={id} />; 
+    return <Sucursal productos={productos} store_id={id} />;
   } catch (error) {
-    console.error('Error al cargar productos:', error);
-    return notFound(); 
+    console.error("Error al cargar productos:", error);
+    return notFound();
   }
 }
