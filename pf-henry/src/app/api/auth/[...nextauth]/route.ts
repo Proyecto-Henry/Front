b0d1c0912317a6288d569db60ae156a4f2e4f10c
Elias Arrieta Hernandez <elias@MacBook-Air.local>
2025-05-14 00:17:30 -0500

    Arreglos 2

--- pf-henry/src/app/api/auth/[...nextauth]/route.ts
+++ pf-henry/src/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { NextAuthOptions } from "next-auth";
 import { ISession } from "@/interfaces/interfaces";
 
-export const nextAuthOptions: NextAuthOptions = {
+const nextAuthOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
