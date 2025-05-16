52ce4d47d06e87bd77dc14c2193baad6c3fe2350
Tomas <tomiiulman@gmail.com>
2025-05-16 17:15:55 -0300

    Proteccion de rutas Users: Admin & sucursal

--- pf-henry/src/app/sucursal/[id]/page.tsx
+++ pf-henry/src/app/sucursal/[id]/page.tsx
@@ -1,13 +1,17 @@
 import Sucursal from '@/components/Sucursal';
+import UserProtectedPage from '@/components/protectedPages/UserProtectedPage';
 
 interface Props {
   params: {
     id: string;
   };
 }
-
 export default async function Page({ params }: Props) {
-  const { id } = params; 
-  
-  return <Sucursal id={id} />;
+  const { id } = params;
+
+  return (
+    <UserProtectedPage>
+      <Sucursal id={id} />;
+    </UserProtectedPage>
+  );
 }
---
+++ pf-henry/src/components/protectedPages/UserProtectedPage.tsx
@@ -0,0 +1,34 @@
+'use client'
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
+import useUserDataStore from "@/store"
+import { Spinner } from "@heroui/react"
+
+const UserProtectedPage = ({ children }: { children: React.ReactNode }) => {
+    const router = useRouter()
+    const { userData, isHydrated } = useUserDataStore()
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        if (!isHydrated) return
+
+        if (!userData?.token || userData?.user?.role !== 'user') {
+            router.replace("/") 
+            return
+        }
+
+        setIsLoading(false)
+    }, [userData, isHydrated, router])
+
+    if (!isHydrated || isLoading) {
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <Spinner />
+            </div>
+        )
+    }
+
+    return <>{children}</>
+}
+
+export default UserProtectedPage
