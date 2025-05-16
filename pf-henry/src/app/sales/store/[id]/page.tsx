908eb3fe76f8a686500a902ccf360d4dcb4247dd
Elias Arrieta Hernandez <elias@MacBook-Air.local>
2025-05-15 22:47:49 -0500

    despliegue 1.4

--- pf-henry/src/app/sales/store/[id]/page.tsx
+++ pf-henry/src/app/sales/store/[id]/page.tsx
@@ -1,7 +1,7 @@
 import HistorySale from "@/components/HistorySale";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = await params;
+  const { id } = params; // Correcto: params es un objeto, no una promesa
 
   return <HistorySale id={id} />;
 }
--- pf-henry/src/app/sucursal/[id]/page.tsx
+++ pf-henry/src/app/sucursal/[id]/page.tsx
@@ -1,7 +1,7 @@
 import Sucursal from "@/components/Sucursal";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = await params;
+  const { id } = params;
 
   return <Sucursal id={id} />;
 }
