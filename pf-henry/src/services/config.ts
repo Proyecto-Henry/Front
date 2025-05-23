d51eeb03527cb6167c2e99aa26630f8179652f5b
Elias Arrieta Hernandez <elias@MacBook-Air.local>
2025-05-22 22:49:51 -0500

    🚀

--- pf-henry/src/services/config.ts
+++ pf-henry/src/services/config.ts
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 export const apiUrl = "https://sistema-de-gestion-back-hf29.onrender.com";
 // export const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-=======
-export const apiUrl = process.env.NEXT_PUBLIC_API_URL;
->>>>>>> 362a5f393a997952817109c44236daa07b33ea5b
